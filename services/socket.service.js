4408009bbf054a54b0ca51e26544ae77ff31a77f
Omer Eshkoli <omere653@gmail.com>
2022-04-03 13:11:37 +0300

    .

--- services/socket.service.js
+++ services/socket.service.js
@@ -6,7 +6,7 @@ var gIo = null
 function connectSockets(http, session) {
     gIo = require('socket.io')(http, {
         cors: {
-            origin: '*',
+            הorigin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
         }
     })
     gIo.on('connection', socket => {
